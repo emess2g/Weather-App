9f1e668fed88a69f3f9b81f5f60d2a71c1d3c036
emess2g <essuonemma84gmail.com>
2024-08-03 03:51:08 +0000

    added styles

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -21,6 +21,18 @@ const Weather = () => {
     fetchData();
   }, []);
 
+
+    // Extract and display weather icon
+    const renderIcon = () => {
+      if (weatherData && weatherData.weather) {
+        const iconCode = weatherData.weather[0].icon;
+        const url = 'http://openweathermap.org'
+        return `${url}/img/w/${iconCode}.png` ;
+      }
+      return null;
+    };
+  
+
   const handleInputChange = (e) => {
     setCity(e.target.value);
   };
@@ -31,30 +43,42 @@ const Weather = () => {
   };
 
   return (
-    <div>
-      
+    <div className='flex flex-col border text-center p- justify-center items-center h-[80vh] w-[] m-4'>
+      <div className="w-[40%]">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter city name"
           value={city}
           onChange={handleInputChange}
+          className='bg-[#202B3B] w-[100%] p-4 rounded-md outline-none' 
         />
-        <button type="submit">Get Weather</button>
+        <button className='xl:hidden' type="submit">Get Weather</button>
       </form>
+      <div className="w-[%]">
       {weatherData ? (
         <>
-          <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Description: {weatherData.weather[0].description}</p>
-          <p>Feels like : {weatherData.main.feels_like}°C</p>
-          <p>Humidity : {weatherData.main.humidity}%</p>
-          <p>Pressure : {weatherData.main.pressure}</p>
-          <p>Wind Speed : {weatherData.wind.speed}m/s</p>
+         <div className="flex flex-col gap-8 justify-center mb-8">
+         <h2 className='text-8xl'>{weatherData.name}</h2>
+           <div className="flex items-center justify-center" >
+           <p className='text-4xl'> {weatherData.main.temp}°C</p>
+           <img className='w-[30%]' src={renderIcon()} alt="" />
+           </div>
+         </div>
+          <p>Description: {weatherData.weather[0].description} </p>
+         
+          <div className="flex text-center text-2xl items-center justify-between mt-8 gap-8 p-4 bg-[#202B3B]">
+          <p>Feels like <br /> {weatherData.main.feels_like}°C</p>
+          <p>Humidity <br /> {weatherData.main.humidity}%</p>
+          <p>Pressure <br /> {weatherData.main.pressure}</p>
+          <p>Wind Speed <br /> {weatherData.wind.speed}m/s</p>
+          </div>
         </>
       ) : (
         <p>Loading weather data...</p>
       )}
+      </div>
+      </div>
     </div>
   );
 };
